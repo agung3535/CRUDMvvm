195fd8cc20bc9c22107b0e0f2d922acac8034433
Saputra <agungdwisaputra0808@gmail.com>
2021-08-14 22:42:07 +0800

    Initial Commit

---
+++ CRUDMvvm/CRUDMvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDMvvmApp.swift
+//  CRUDMvvm
+//
+//  Created by Putra on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct CRUDMvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CRUDMvvm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CRUDMvvm
+//
+//  Created by Putra on 14/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
